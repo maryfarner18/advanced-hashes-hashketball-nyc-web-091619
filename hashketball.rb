635bca4ad9fd1b80a53c80693cc0621c86e33cd4
Mary Farner <maryfarner@gmail.com>
2019-08-27 02:33:17 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -10,11 +10,14 @@ def game_hash
       colors: ["Black", "White"],
       players: [ 
         { name: "Alan Anderson", stats: { number: 0, shoe: 16, points: 22, rebounds: 12, assists: 12, steals: 3, blocks: 1, slam_dunks: 1}},
+        
         { name: "Reggie Evans", stats: { number: 30, shoe: 14, points: 12, rebounds: 12, assists: 12, steals: 12, blocks: 12, slam_dunks: 7}},
+        
         { name: "Brook Lopez", stats: { number: 11, shoe: 17, points: 17, rebounds: 19, assists: 10, steals: 3, blocks: 1, slam_dunks: 15}},
+        
         { name: "Mason Plumlee", stats: { number: 1, shoe: 19, points: 26 rebounds: 11, assists: 6, steals: 3, blocks: 8, slam_dunks: 5}},
+        
         { name: "Jason Terry", stats: { number: 31, shoe: 15, points: 19, rebounds: 2, assists: 2, steals: 4, blocks: 11, slam_dunks: 1}}
-       
         ]
     },
     away: {
@@ -23,10 +26,12 @@ def game_hash
       players: [
         { name: "Jeff Adrien", stats: { number: 4, shoe: 18, points: 10, rebounds: 1, assists: 1, steals: 2, blocks: 7, slam_dunks: 2}},
         { name: "Bismack Biyombo", stats: { number: 0, shoe: 17, points: 17, rebounds: 19, assists: 10, steals: 3, blocks: 1, slam_dunks: 15}},
+        
         { name: "DeSagna Diop", stats: { number: 2, shoe: 14, points: 24 rebounds: 12, assists: 12, steals: 4, blocks: 5, slam_dunks: 5}},
+        
         { name: "Ben Gordon", stats: { number: 8, shoe: 15, points: 33, rebounds: 3, assists: 2, steals: 1, blocks: 1, slam_dunks: 0}},
+        
         { name: "Kemba Walker", stats: { number: 33, shoe: 15, points: 6, rebounds: 12, assists: 12, steals: 7, blocks: 5, slam_dunks: 12}}
-          
         ]
     } 
     
