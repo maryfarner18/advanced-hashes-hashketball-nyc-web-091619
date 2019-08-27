90138c998857c98f7c3510085f619538a1091a9d
Mary Farner <maryfarner@gmail.com>
2019-08-27 03:01:17 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -112,7 +112,7 @@ end
 def most_points_scored
   most_scored = {}
   max = 0 
-  
+  puts "hi"
   game_hash.each do |location, team_data| 
     team_data[:players].each do |player|
       if player[:stats][:points] > max then
