fe57af179b0260dc1c0deb96acd400cbc5775e7e
Mary Farner <maryfarner@gmail.com>
2019-08-27 02:32:19 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -22,9 +22,10 @@ def game_hash
       colors: ["Turquoise", "Purple"],
       players: [
         { name: "Jeff Adrien", stats: { number: 4, shoe: 18, points: 10, rebounds: 1, assists: 1, steals: 2, blocks: 7, slam_dunks: 2}},
-        { name: "Bismack Biyombo", stats: { number: 11, shoe: 17, points: 17, rebounds: 19, assists: 10, steals: 3, blocks: 1, slam_dunks: 15}},
-        { name: "DeSagna Diop", stats: { number: 1, shoe: 19, points: 26 rebounds: 11, assists: 6, steals: 3, blocks: 8, slam_dunks: 5}},
-        { name: "Ben Gordon", stats: { number: 31, shoe: 15, points: 19, rebounds: 2, assists: 2, steals: 4, blocks: 11, slam_dunks: 1}}
+        { name: "Bismack Biyombo", stats: { number: 0, shoe: 17, points: 17, rebounds: 19, assists: 10, steals: 3, blocks: 1, slam_dunks: 15}},
+        { name: "DeSagna Diop", stats: { number: 2, shoe: 14, points: 24 rebounds: 12, assists: 12, steals: 4, blocks: 5, slam_dunks: 5}},
+        { name: "Ben Gordon", stats: { number: 8, shoe: 15, points: 33, rebounds: 3, assists: 2, steals: 1, blocks: 1, slam_dunks: 0}},
+        { name: "Kemba Walker", stats: { number: 33, shoe: 15, points: 6, rebounds: 12, assists: 12, steals: 7, blocks: 5, slam_dunks: 12}}
           
         ]
     } 
