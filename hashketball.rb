8b1f3c4d8efe773b2773f886c7973f2c20cef1c6
Mary Farner <maryfarner@gmail.com>
2019-08-27 02:33:44 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -15,7 +15,7 @@ def game_hash
         
         { name: "Brook Lopez", stats: { number: 11, shoe: 17, points: 17, rebounds: 19, assists: 10, steals: 3, blocks: 1, slam_dunks: 15}},
         
-        { name: "Mason Plumlee", stats: { number: 1, shoe: 19, points: 26 rebounds: 11, assists: 6, steals: 3, blocks: 8, slam_dunks: 5}},
+        { name: "Mason Plumlee", stats: { number: 1, shoe: 19, points: 26, rebounds: 11, assists: 6, steals: 3, blocks: 8, slam_dunks: 5}},
         
         { name: "Jason Terry", stats: { number: 31, shoe: 15, points: 19, rebounds: 2, assists: 2, steals: 4, blocks: 11, slam_dunks: 1}}
         ]
