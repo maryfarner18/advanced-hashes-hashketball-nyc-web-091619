7d3da46027cf186494272ceca1b048b7236e88cf
Mary Farner <maryfarner@gmail.com>
2019-08-27 02:27:04 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -32,7 +32,7 @@ def num_points_scored(name)
   game_hash.each do |location, team_data|
     game_hash[location][:players].each_with_index do |player, index|
       if player[:name] == name then
-        return player[:points]
+        return player[:stats][:points]
       end
     end
   end
