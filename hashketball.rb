355adfa9014329c623c56b37f7a887d5d005e968
Mary Farner <maryfarner@gmail.com>
2019-08-27 02:28:44 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -11,8 +11,8 @@ def game_hash
       players: [ 
         { name: "Alan Anderson", stats: { number: 0, shoe: 16, points: 22, rebounds: 12, assists: 12, steals: 3, blocks: 1, slam_dunks: 1}},
         { name: "Reggie Evans", stats: { number: 30, shoe: 14, points: 12, rebounds: 12, assists: 12, steals: 12, blocks: 12, slam_dunks: 7}},
-        { name: "Reggie Evans", stats: { number: 30, shoe: 14, points: 12, rebounds: 12, assists: 12, steals: 12, blocks: 12, slam_dunks: 7}},
-        { name: "Reggie Evans", stats: { number: 30, shoe: 14, points: 12, rebounds: 12, assists: 12, steals: 12, blocks: 12, slam_dunks: 7}},
+        { name: "Brook Lopez", stats: { number: 30, shoe: 14, points: 12, rebounds: 12, assists: 12, steals: 12, blocks: 12, slam_dunks: 7}},
+        { name: "Mason Plumlee", stats: { number: 30, shoe: 14, points: 12, rebounds: 12, assists: 12, steals: 12, blocks: 12, slam_dunks: 7}},
         { name: "Reggie Evans", stats: { number: 30, shoe: 14, points: 12, rebounds: 12, assists: 12, steals: 12, blocks: 12, slam_dunks: 7}}
        
         ]
